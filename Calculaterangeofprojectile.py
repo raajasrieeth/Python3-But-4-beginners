10c155c1b452bfcc2c3a208c18748f9eb9dff3ef
Raaj Asrieeth MR <asrieethraaj@gmail.com>
2020-10-02 11:53:00 +0530

    Update Calculaterangeofprojectile.py

--- Calculaterangeofprojectile.py
+++ Calculaterangeofprojectile.py
@@ -2,6 +2,6 @@ import math
 
 vi = float(input("Enter the initial velocity"))
 theta = float(input("Enter the angle at which the projectile was launched in degrees."))
-Range = vi**2 * 2*math.sin(theta)/ 9.8
+height = (vi**2 * (math.sin(theta)*math.sin(theta)))/ (2*9.8)
 
-print("Max Range, " Range )
+print(height)
